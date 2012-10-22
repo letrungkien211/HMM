8e8d6dcfbe76621238267fbf79ab195f67501d5b
letrungkien211 <letrungkien211@gmail.com>
2012-10-22 18:38:06 +0900

    Decode done

--- hmm.hpp
+++ hmm.hpp
@@ -1,3 +1,7 @@
+/*
+ * Hidden Markov Model
+ */
+
 #ifndef HMM_HPP
 #define HMM_HPP
 
@@ -25,8 +29,9 @@ public:
 	MatrixXd Forward(const MatrixXi &observation, MatrixXd &scale);
 	MatrixXd Backward(const MatrixXi &observation, const MatrixXd &scale);
 	MatrixXi Decode(const MatrixXi &observation, double &probability);
-	double Learn(vector<MatrixXi> &observation, int iterations, double tolerance);
+	double Learn(vector<MatrixXi> &observations, int iterations, double tolerance);
 	void Reset();
+	bool CheckConvergen();
 
 	// private members access
 	int N() const;
@@ -40,5 +45,29 @@ public:
 	MatrixXd &B();
 	MatrixXd &PI();
 };
+
+template <class T>
+class MyMatrix3x{
+private:
+	int width, height;
+	vector<T> data;
+public:
+	MyMatrix3x():width(0), height(0){
+	}
+	MyMatrix3x(int m, int n, int p): width(m), height(n){
+		resize(m,n,p);
+	}
+	void resize(int m, int n, int p){
+		data.resize(m*n*p);
+	}
+	T operator()(int i, int j, int k) const{
+		return data[i + j*width + k*width*height];
+	}
+	T& operator()(int i, int j, int k){
+		return data[i + j*width + k*width*height];
+	}
+};
+
+typedef MyMatrix3x<double> MyMatrix3d;
 }
 #endif
