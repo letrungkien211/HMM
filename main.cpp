3eb163a088421b00e7be449b58df5e2326914ca4
letrungkien211 <letrungkien211@gmail.com>
2012-10-23 22:17:15 +0900

    Finished: Forward, Evaluate Unfinished: Backward, Decode, Learning

--- main.cpp
+++ main.cpp
@@ -12,15 +12,17 @@
 
 using namespace std;
 using namespace ltk;
+
 int main(int argc, char* argv[]){
 	HMM hmm(2,2);
 	hmm.A() << 0.5,0.5,0.0,1.0;
 	hmm.B() << 0.5,0.5,0.5,0.5;
-	hmm.PI() << 0.5,  0.5, 0.5;
-	MatrixXi ob(3,1);
+	hmm.PI() << 0.75,  0.25;
+	MatrixXi ob(2,1);
 	MatrixXd scale;
 
-	ob << 0,0,1;
-	hmm.Forward(ob, scale);
+	ob << 1,0;
+	cout<< hmm.Forward(ob, scale)<<endl;
+	cout<< hmm.Evaluate(ob)<<endl;
 	return 0;
 }
