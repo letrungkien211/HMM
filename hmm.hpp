3e7d95cdfab33d13b232274bb50d93e4de31e8e6
letrungkien211 <letrungkien211@gmail.com>
2012-10-25 19:25:09 +0900

    Change Makefile to remove object files

--- hmm.hpp
+++ hmm.hpp
@@ -15,25 +15,26 @@ using namespace Eigen;
 
 class HMM{
 private:
-	int n;  // number of states
-	int m;  // number of observation in alphabet
-	MatrixXd a;  // state transition probability distribution nxn
-	MatrixXd b;  // observation symbol probability distribution nxm
-	MatrixXd pi; // initial state distribution nx1
-
+	int n;  // Number of states
+	int m;  // Number of observation in alphabet
+	MatrixXd a;  // State transition probability distribution nxn
+	MatrixXd b;  // Observation symbol probability distribution nxm
+	MatrixXd pi; // Initial state distribution nx1
+	MatrixXd Forward(const MatrixXi &observation, MatrixXd &scale);  // Forward calculation
+	MatrixXd Backward(const MatrixXi &observation, const MatrixXd &scale); // Backward calculation
+	bool CheckConvergence(double oldLikelihood, double newLikelihood,
+			int currentIteration, int maxIteration, double tolerance); // Check convergence in iteration process
 public:
-	HMM(int n_, int m_);
-	~HMM();
+	HMM(int n_, int m_); // Constructor
+	~HMM();  // Destructor
 	// Methods
-	double Evaluate(const MatrixXi &observation, bool logarithm = true);
-	MatrixXd Forward(const MatrixXi &observation, MatrixXd &scale);
-	MatrixXd Backward(const MatrixXi &observation, const MatrixXd &scale);
-	MatrixXi Decode(const MatrixXi &observation, double &probability);
-	double Learn(vector<MatrixXi> &observation, int maxIteration, double tolerance);
-	bool CheckConvergence(double oldLikelihood, double newLikelihood, int currentIteration, int maxIteration, double tolerance);
+	double Evaluate(const MatrixXi &observation, bool logarithm = true); // P(O|a,b,pi)
+	MatrixXi Decode(const MatrixXi &observation, double &probability); // q= argmax P(O|a,b,pi)
+	double Learn(vector<MatrixXi> &observation, int maxIteration, double tolerance); // (a,b,pi) = argmax P(O|a,b,pi)
 
-	void Initialize(int n_, int m_);
-	// private members access
+	void Initialize(int n_, int m_); // Initialization
+
+	// Private members access
 	int N() const;
 	int M() const;
 	MatrixXd A() const;
